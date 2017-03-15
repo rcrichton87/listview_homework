f01c73013c8ab1ee7b43f3eeae575b44ddb63c2c
Ross Crichton <rcrichton87@googlemail.com>
2017-03-15 18:20:37 +0000

    TopPilotsActivity created

---
+++ app/src/main/java/com/codeclan/pilotrankings/TopPilotsActivity.java
@@ -0,0 +1,24 @@
+package com.codeclan.pilotrankings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class TopPilotsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pilot_list);
+        TopPilots topPilots = new topPilots();
+        ArrayList<Pilot> list = topPilots.getList();
+
+        TopPilotsAdapter pilotAdapter = new TopPilotsAdapter(this, list);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(pilotAdapter);
+    }
+
+}
